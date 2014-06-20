785b7168436e3e28e4b896f32eff442639777edd
ryan <rjs007@live.com>
2014-06-20 13:49:42 -0400

    Mod Class created

---
+++ src/main/java/com/mimer29or40/firstmod/FirstMod.java
@@ -0,0 +1,31 @@
+package com.mimer29or40.firstmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="FirstMod",name="First Mod",version="1.7.2-0.1")
+public class FirstMod
+{
+    @Mod.Instance("FirstMod")
+    public static FirstMod instance;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLInitializationEvent event)
+    {
+
+    }
+}
