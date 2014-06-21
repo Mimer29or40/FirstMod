efbf2e3ee136fc26512cc89a3126e48f9f2dd1f5
ryan <rjs007@live.com>
2014-06-20 22:23:06 -0400

    Added a reference file

---
+++ src/main/java/com/mimer29or40/firstmod/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.mimer29or40.firstmod.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "firstMod";
+    public static final String MOD_NAME = "First Mod";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final String VERSION = "@VERSION@";
+}
