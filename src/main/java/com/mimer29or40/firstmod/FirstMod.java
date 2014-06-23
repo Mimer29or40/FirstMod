608728e7b1596fd5a3831c85514373f2c83cceb5
ryan <rjs007@live.com>
2014-06-23 13:29:17 -0400

    Removes Fingerprint

--- src/main/java/com/mimer29or40/firstmod/FirstMod.java
+++ src/main/java/com/mimer29or40/firstmod/FirstMod.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, certificateFingerprint = Reference.FINGERPRINT)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class FirstMod
 {
     @Mod.Instance(Reference.MOD_ID)
--- src/main/java/com/mimer29or40/firstmod/reference/Reference.java
+++ src/main/java/com/mimer29or40/firstmod/reference/Reference.java
@@ -4,6 +4,5 @@ public class Reference
 {
     public static final String MOD_ID = "firstMod";
     public static final String MOD_NAME = "First Mod";
-    public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String VERSION = "@VERSION@";
 }
