5a3a4ca3e4040cfc8a2b09a976c9ae2d5f4970c5
ryan <rjs007@live.com>
2014-07-02 13:10:34 -0400

    Implemented LogHelper and added in-game config changing

--- src/main/java/com/mimer29or40/firstmod/FirstMod.java
+++ src/main/java/com/mimer29or40/firstmod/FirstMod.java
@@ -9,6 +9,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class FirstMod
